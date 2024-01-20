7024bbde74258e4e21410da355f17cbcc4fd773b
Wyatt-Desktop <WyattHansen99@gmail.com>
2024-01-19 19:36:44 -0600

    Completed 14TaskScheduler

--- 14TaskScheduler/Inc/main.h
+++ 14TaskScheduler/Inc/main.h
@@ -11,7 +11,8 @@
 
 // MACROS
 
-#define MAX_TASKS				4
+// 4 User Tasks + 1 Idle Task
+#define MAX_TASKS				5
 #define DUMMY_XPSR				0x01000000U
 
 // Stack Memory Calculations for 1kb each
@@ -26,14 +27,19 @@
 #define T2_STACK_START			((T1_STACK_START) - (SIZE_TASK_STACK))
 #define T3_STACK_START			((T2_STACK_START) - (SIZE_TASK_STACK))
 #define T4_STACK_START			((T3_STACK_START) - (SIZE_TASK_STACK))
-#define SCHEDULER_STACK_START	((T4_STACK_START) - (SIZE_SCHEDULER_STACK))
+#define IDLE_STACK_START		((T4_STACK_START) - (SIZE_TASK_STACK))
+#define SCHEDULER_STACK_START	((IDLE_STACK_START) - (SIZE_SCHEDULER_STACK))
 
 #define TICK_HZ					1000U
 #define HSI_CLOCK				16000000U
 #define SYSTICK_TIMER_CLOCK		HSI_CLOCK
 
-#define TASK_RUNNING_STATE 0x00
+#define TASK_READY_STATE 0x00
 #define TASK_BLOCKED_STATE 0xFF
+
+// Interrupt Disable using the Priority Mask Register
+#define INTERRUPT_DISABLE() do{__asm volatile ("MOV R0,#0x01"); __asm volatile ("MSR PRIMASK,R0");}while(0)
+#define INTERRUPT_ENABLE() do{__asm volatile ("MOV R0,#0x00"); __asm volatile ("MSR PRIMASK,R0");}while(0)
 
 
 
